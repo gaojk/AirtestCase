4825ff4535e39c79206d1022464e890bf2e2a353
刘树栋 <664299087@qq.com>
2018-12-11 10:52:30 +0800

    创建AirtestCase项目

---
+++ AirtestCase/用例集/报表-金额统计.air/报表-金额统计.py
@@ -0,0 +1,11 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+touch(Template(r"tpl1543566014821.png", record_pos=(0.1, -0.235), resolution=(1366, 768)))
+sleep(1.0)
+
+touch(Template(r"tpl1543566039692.png", record_pos=(0.454, -0.179), resolution=(1366, 768)))
+touch(Template(r"tpl1543647956764.png", record_pos=(-0.343, -0.234), resolution=(1366, 768)))
