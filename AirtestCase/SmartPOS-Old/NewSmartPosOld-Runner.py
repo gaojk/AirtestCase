09b1a33b967c4652a853acacc8117f702c834b72
刘树栋 <664299087@qq.com>
2019-01-08 14:48:01 +0800

    allcase_list.py NewSmartPosOld-Runner.py summary-log summary-log/summary_template.html

---
+++ AirtestCase/SmartPOS-Old/NewSmartPosOld-Runner.py
@@ -0,0 +1,93 @@
+from airtest.cli.runner import AirtestCase, run_script
+from argparse import *
+import airtest.report.report as report
+import jinja2
+import shutil
+import os
+import io
+import time
+import allcase_list  # 调用数组文件
+
+alltestnames = allcase_list.shop_list()
+print(alltestnames)
+
+
+class CustomAirtestCase(AirtestCase):
+    def setUp(self):
+        print("custom setup")
+        # add var/function/class/.. to globals
+        # self.scope["hunter"] = "i am hunter"
+        # self.scope["add"] = lambda x: x+1
+
+        # exec setup script
+        # self.exec_other_script("setup.owl")
+        super(CustomAirtestCase, self).setUp()
+
+    def tearDown(self):
+        print("custom tearDown")
+        # exec teardown script
+        # self.exec_other_script("teardown.owl")
+        super(CustomAirtestCase, self).setUp()
+
+    def run_air(self, root_dir='D:\\tools\\airtestCase\\案例集', device=['android://127.0.0.1:5037/99.12.74.40:7281']):
+        # 聚合结果
+        results = []
+        # 获取所有用例集
+        root_log = root_dir + '\\' + 'log'
+        if os.path.isdir(root_log):
+            shutil.rmtree(root_log)
+        else:
+            os.makedirs(root_log)
+            print('log文件夹位置=' + str(root_log))
+        for f in alltestnames:
+            if f.endswith(".air"):
+                # f为.air案例名称：手机银行.air
+                airName = f
+                script = os.path.join(root_dir, f)
+                # airName_path为.air的全路径：D:\tools\airtestCase\案例集\log\手机银行
+                print('执行用例全路径='+script)
+                # 日志存放路径和名称：D:\tools\airtestCase\案例集\log\手机银行1
+                log = os.path.join(root_dir, 'log' + '\\' + airName.replace('.air', ''))
+                print('用例log保存文件夹=' + log)
+                if os.path.isdir(log):
+                    shutil.rmtree(log)
+                else:
+                    os.makedirs(log)
+                    print('log保存路径=' + str(root_log))
+                output_file = log + '\\' + 'log.html'
+                args = Namespace(device=device, log=log, recording=None, script=script)
+                try:
+                    run_script(args, AirtestCase)
+                except:
+                    pass
+                finally:
+                    rpt = report.LogToHtml(script, log)
+                    rpt.report("log_template.html", output_file=output_file)
+                    result = {}
+                    result["name"] = airName.replace('.air', '')
+                    result["result"] = rpt.test_result
+                    results.append(result)
+        # 生成聚合报告
+        root_dir_summary = 'E:\\AirtestCase\\SmartPOS-Old\\summary-log'
+        print(root_dir)
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(root_dir_summary),
+            extensions=(),
+            autoescape=True
+        )
+        now = time.strftime("%Y%m%d-%H%M%S", time.localtime(time.time()))
+        summary_name=now+"summary.html"
+
+        template = env.get_template("summary_template.html", root_dir_summary)
+        html = template.render({"results": results})
+        output_file = os.path.join(root_dir_summary, summary_name)
+        print('summary全路径=' + output_file)
+        with io.open(output_file, 'w', encoding="utf-8") as f:
+            f.write(html)
+
+
+if __name__ == '__main__':
+    test = CustomAirtestCase()
+    device = ['android:2d87aa41']
+    # device = ['android:127.0.0.1:62001']
+    test.run_air('E:\\AirtestCase\\SmartPOS-Old\\用例集', device)
---
+++ AirtestCase/SmartPOS-Old/allcase_list.py
@@ -0,0 +1,21 @@
+# untie执行用例列表
+
+# shop执行用例列表
+def shop_list():
+    alltestnames = [
+        # 'cloudshop_case.shop_login_old.Login',
+        # 'cloudshop_case.shop_register.Register',
+        # 'cloudshop_case.shop_goods_search.Search',
+
+        # 'cloudshop_case.shop_good_class.Goodclass',
+        # 'cloudshop_case.shop_add_goods.Addgoods',
+        # 'cloudshop_case.shop_good_property.GoodProperty',
+        # 'cloudshop_case.shop_upload_pic.Uploadpic',
+        # 'cloudshop_case.shop_add_employee.AddEmployee',
+        # 'cloudshop_case.shop_add_table.AddTable',
+        # '简餐-下单-会员支付.air',
+        '简餐-下单-打包费.air',
+
+    ]
+    print("成功读取[shop执行用例列表]")
+    return alltestnames
